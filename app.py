28414d09735c026c077d3309922137875775fd1f
almondCabbage <almond.cabbage@gmail.com>
2022-07-19 14:19:09 +0500

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import flask
 import pickle
-
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDRegressor
+from sklearn.multioutput import MultiOutputRegressor
 
 # Use pickle to load in the pre-trained model.
 with open(f'model/tfidf_vectorizer.pkl', 'rb') as file:
@@ -8,12 +13,17 @@ with open(f'model/tfidf_vectorizer.pkl', 'rb') as file:
     
 with open(f'model/classifier.pkl', 'rb') as file:
     classifier = pickle.load(file)
+    
+
+    
+with open(f'model/applause_regressor.pkl', 'rb') as file:
+    applause_regressor = pickle.load(file)
 
 def predict_category(text):
     result = classifier.predict(tfidf_vectorizer.transform([text]))
     return(result[0])
 
-
+parties = ['90/Die', 'LINKE', 'FDP', 'CDU/CSU', 'SPD', 'AfD']
     
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -26,10 +36,13 @@ def main():
         news_text = flask.request.form['news_text']
         prediction = predict_category(news_text)
         
-        
+        applause_value = applause_regressor.predict(["news_text"])
+        abc = []
+        for p,applause in zip(parties,applause_value.T):
+            abc.append(f'applause_predicted-{p} ='+ str(applause))
         return flask.render_template('main.html',
                                      original_input={'News Text':news_text},
-                                     result=prediction)
+                                     result=[prediction, abc])
 
 
     
